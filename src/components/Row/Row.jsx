8bfba0daf6b9f540742be64b86d224176c43845e
nomiboy <noumanijaz.dev@outlook.com>
2021-12-22 20:57:36 +0500

    finalized the App and Deploy

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
-import YouTube from "react-youtube";
+
 import axios from "../../axios";
-import "./Row.css"
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_Url = "http://image.tmdb.org/t/p/original/";
 
-
 function Row({ title, fetchUrl, isLargeRow }) {
 	const [movies, setMovies] = useState([]);
+	const [trailerUrl, setTrailerUrl] = useState("")
+
+	const handleClick = (movie) => {
+		if (trailerUrl) {
+			 setTrailerUrl("")
+		} else {
+			movieTrailer(movie?.name || "")
+				.then(url => {
+					//https://www.youtube.com/watch?v=...
+					const urlParams = new URLSearchParams(new URL(url).search)
+					setTrailerUrl(urlParams.get("v"));
+			}).catch(error => console.log(error))
+		 }
+	 }
 
 	useEffect(() => {
 		async function fetchData() {
@@ -19,20 +34,33 @@ function Row({ title, fetchUrl, isLargeRow }) {
 	}, [fetchUrl]);
 	console.log(movies);
 
+	const opts = {
+		height: "390",
+		width: "100%",
+		playerVars: {
+			// https://developers.google.com/youtube/player_parameters
+			autoplay: 1,
+		},
+	};
+
+
 	return (
 		<div className="row">
 			<h1>{title}</h1>
 			<div className="row-posters">
 				{movies.map((movie) => (
-					<img 
+					<img
 						key={movie.id}
-						className={`row-poster ${isLargeRow&& "row-posterLarge"}`}
-						src={`${base_Url}${isLargeRow? movie.poster_path: movie.backdrop_path}`}
+						onClick={()=> handleClick(movie)}
+						className={`row-poster ${isLargeRow && "row-posterLarge"}`}
+						src={`${base_Url}${
+							isLargeRow ? movie.poster_path : movie.backdrop_path
+						}`}
 						alt={movie.name}
 					/>
 				))}
 			</div>
-			<YouTube videoId={trailerUrl } opts={opts}/>
+			{trailerUrl&&<YouTube videoId={trailerUrl} opts={opts} />}
 		</div>
 	);
 }
