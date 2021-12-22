3f9d6e5957ca231fe4c5d4edb0d48312133f2e9d
nomiboy <noumanijaz.dev@outlook.com>
2021-12-22 19:20:46 +0500

    Completed with Navbar components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Navbar.css"
 
 function Navbar() {
+    const [isShow, setIsShow] = useState(false)
+  
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setIsShow(true)
+            }else setIsShow(false)
+        })
+        return () => {
+            window.removeEventListener("scroll")
+        }
+    }, [])
+
     return (
-        <div className='navbar'>
-            <h1>Netflix</h1>
-        </div>
-    )
+			<div className={`navbar ${isShow && "navbar-black"} `}>
+				<img
+					className="nav-logo"
+					src="https://logos-download.com/wp-content/uploads/2016/03/Netflix_Logo_2014-1536x413.png"
+					alt=""
+				/>
+				<img
+					className="navbar-avatar"
+					src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+					alt=""
+				/>
+			</div>
+		);
 }
 
 export default Navbar
